77288793abb3b38259b890e2b17450b505dd1a38
sxs <sxs@weltraumschaf.fritz.box>
2014-05-03 15:06:45 +0200

    Move files in source dir.

--- install.sh
+++ install.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
+##
+## Intsalls the dotfiles into $HOME by softlinking them.
+## Existing files are backupped.
+##
+
+sourceDir="src/_*"
+
+##
+## Links source file into target directory.
+## Makes backup if target link already exists.
+##
+## @param $1 source script
+## @param $2 target direcotry
+##
 function linkFile {
     source="${PWD}/$1"
-    target="${HOME}/${1/_/.}"
+    targetFile="${1/src\//}"
+    target="${1/_/.}"
+    target="${2}/${targetFile}"
 
     # Only create backup if target is a file or directory
     if [ -f "${target}" ] || [ -d "${target}" ]; then
-        mv -v "$target" "$target.bak"
+        if [ ! -L "${target}" ]; then
+            echo "Backing up ${target}"
+            mv -v "${target}" "${target}.bak"
+        fi
     fi
 
     ln -svf "${source}" "${target}"
 }
 
-for i in _*
+for file in sourceDir
 do
-  linkFile "$i"
+  linkFile "${file}" "${HOME}"
 done
+
+echo "Finished :)"
